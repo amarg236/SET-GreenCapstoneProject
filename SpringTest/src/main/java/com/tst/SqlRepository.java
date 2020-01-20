1e4d3937418b0393bc241330fa25db9e10512b18
brendon-lebaron <59894426+brendon-lebaron@users.noreply.github.com>
2020-01-20 16:23:08 -0600

    Delete SqlRepository.java

--- SpringTest/src/main/java/com/tst/SqlRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tst;
-
-import org.springframework.data.repository.CrudRepository;
-import com.tst.SqlTest; //Importing our object to be stored
-//This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-//CRUD refers Create, Read, Update, Delete
-public interface SqlRepository extends CrudRepository<SqlTest, Integer>{
-
-}
