a1fdf1475ea2ee6282d401ec51c7a7575b576638
Sonam Gurung <gurungamar25@gmail.com>
2020-01-30 16:16:50 -0600

    deletd back those admincontrol repo and service

--- SET-Green Capstone Project/src/main/java/com/setgreen/setgreen/services/AdminControlService.java
+++
@@ -1,9 +0,0 @@
-package com.setgreen.setgreen.services;
-
-import com.setgreen.setgreen.model.User;
-
-import java.util.List;
-
-public interface AdminControlService {
-    List<User> showUnverifiedUsers();
-}
