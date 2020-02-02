e50691eec74cc8d3fc2827612f0612b1a79fd28c
brendon-lebaron <59894426+brendon-lebaron@users.noreply.github.com>
2020-02-02 10:05:57 -0600

    email support
    moving over relevant code, trying real hard not to break anything.

---
+++ SET-Green Capstone Project/src/main/java/com/setgreen/setgreen/model/Mail/Mail.java
@@ -0,0 +1,11 @@
+package com.setgreen.setgreen.model.Mail;
+
+import lombok.Data;
+
+@Data
+public class Mail {
+    public String sendTo;
+    public String subjectLine;
+    public String emailContent;
+    public Mail() {}
+}
