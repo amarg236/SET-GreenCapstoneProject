193eb355000e8cf1b9df399f9a6aff4861d5b5fa
Sonam Gurung <gurungamar25@gmail.com>
2020-02-05 00:31:55 -0600

    Revert "Revert "Revert "undoing changes"""
    This reverts commit cb64ccdb11b5e0a4a9295d433128dcc28748b64b.

--- front-end-with-react/src/App.js
+++ front-end-with-react/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 
@@ -11,28 +10,12 @@ import ViewApiCall from "./components/Project/ViewApiCall";
 import SignIn from "./components/Project/SignIn";
 import CreateGame from "./components/Game/CreateGame";
 import ViewGame from "./components/Game/ViewGame";
-=======
-import Header from "./components/Layout/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import AddProject from "./components/Project/AddProject";
-import ApproveGame from "./components/Project/ApproveGame";
-import SignIn from "./components/Project/SignIn";
-import Home from "./components/Layout/Home";
-import CreateGame from "./components/Project/CreateGame";
-import Footer from "./components/Layout/Footer";
->>>>>>> e505ed2037c1514a6da0dcc3408858c573ca45ef
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-<<<<<<< HEAD
 
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/viewApiCall" component={ViewApiCall} />
@@ -40,21 +23,6 @@ function App() {
         <Route exact path="/viewGame" component={ViewGame} />
         <Route exact path="/addProject" component={AddProject} />
         <Route exact path="/signIn" component={SignIn} />
-=======
-        
-        <Switch> 
-        <Route exact path="/createGame" component={CreateGame} />
-        <Route exact path="/ApproveGame" component={ApproveGame} />
-        <Route exact path="/addProject" component={AddProject} />
-        <Route exact path="/signIn" component={SignIn} />
-        <Route exact path="/home" component={Home} />
-        <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        
-        <Footer />
->>>>>>> e505ed2037c1514a6da0dcc3408858c573ca45ef
       </div>
     </Router>
   );
