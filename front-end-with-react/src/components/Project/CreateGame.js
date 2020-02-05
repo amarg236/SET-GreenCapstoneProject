e2fb0335d5743088419944bc13d7314bd4cfb216
Sonam Gurung <gurungamar25@gmail.com>
2020-02-05 00:31:31 -0600

    Revert "Revert "undoing changes""
    This reverts commit 2ca3c28017e98f323f9b6afdf3ee11bc2009600e.

---
+++ front-end-with-react/src/components/Project/CreateGame.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "../../stylesheets/createGame.css";
+
+class CreateGame extends Component {
+  render() {
+    return (
+    <div className='gameLayout'>
+      <form className='auth-inner'>
+                <h3 style={{textAlign:'center'}}>
+                    Create Game</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+            </div>
+    );
+  }
+}
+
+export default CreateGame;
