2d864a7c247758bbc4bf150d0637f42b095f2993
marissamizell1 <mmizell1234@gmail.com>
2020-02-06 18:02:02 -0600

    collapsable navbar

--- front-end-with-react/src/Utility/AuthService.js
+++ front-end-with-react/src/Utility/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_API_BASE_URL = "http://localhost:8080";
+const USER_API_BASE_URL = "http://ec2-3-17-66-87.us-east-2.compute.amazonaws.com:8080";
 
 class AuthService {
   login(credentials) {
