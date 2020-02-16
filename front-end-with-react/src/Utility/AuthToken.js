3d7244c6e3145e4bdcf590fe609a773c9d574707
Sonam Gurung <gurungamar25@gmail.com>
2020-02-16 17:08:56 -0600

    just change of url - not bit deal

--- front-end-with-react/src/Utility/AuthToken.js
+++ front-end-with-react/src/Utility/AuthToken.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const USER_API_BASE_URL = "http://localhost:8080";
-// "http://ec2-3-17-66-87.us-east-2.compute.amazonaws.com:8080";
+const USER_API_BASE_URL =
+  // "http://localhost:8080";
+  "http://ec2-3-17-66-87.us-east-2.compute.amazonaws.com:8080";
 
 class AuthToken {
   authentication = {
