2c5ccdd0e3a365c386dc557b1303c337a9caaa5d
marissamizell1 <mmizell1234@gmail.com>
2020-03-01 15:14:30 -0600

    Changed available views and added excel export

--- front-end-with-react/src/components/Project/Cal.js
+++ front-end-with-react/src/components/Project/Cal.js
@@ -5,8 +5,10 @@ import {
   ScheduleComponent,
   Day,
   Week,
-  WorkWeek,
-  Month
+  ExcelExport,
+  ViewsDirective,
+  Month,
+  ViewDirective
 } from "@syncfusion/ej2-react-schedule";
 import { extend } from "@syncfusion/ej2-base";
 import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
@@ -17,28 +19,43 @@ class Cal extends React.Component {
     this.data = extend([], dataSource.EventsData, null, true);
   }
 
+  onActionBegin(args) {
+    if (args.requestType === 'toolbarItemRendering') {
+      let exportItem = {
+        align: 'Right', showTextOn: 'Both', prefixIcon: 'e-icon-schedule-excel-export',
+        text: 'Excel Export', cssClass: 'e-excel-export', click: this.onExportClick.bind(this)
+      };
+      args.items.push(exportItem);
+    }
+  }
+  onExportClick() {
+    this.scheduleObj.exportToExcel();
+  }
+
   // Links that could be helpful
   // https://github.com/syncfusion/ej2-react-samples/blob/master/src/schedule/local-data.jsx
 
-  // data = new DataManager([], dataSource.eventsData, null, true);
-  // Schedule remote data
-  // remoteData = new DataManager({
-  //   dataSource: "../dummy.json"
-  //   // url: " ",
-  //   // adaptor: new WebApiAdaptor(),
-  //   // crossDomain: true
-  // });
 
   render() {
     return (
       <ScheduleComponent
+        cssClass='excel-export'
         currentView="Month"
         eventSettings={{ dataSource: this.data }}
-        // selectedDate={new Date(2020, 2, 20)}
-        style={{ maxHeight: "80vh",
-                 minWidth: '40vh', }}
+        id='schedule'
+        ref={t => this.scheduleObj = t}
+        actionBegin={this.onActionBegin.bind(this)}
+        style={{
+          maxHeight: "80vh",
+          minWidth: '40vh',
+        }}
       >
-        <Inject services={[Day, Week, WorkWeek, Month]} />
+        <ViewsDirective>
+          <ViewDirective option='Month' />
+          <ViewDirective option='Week' />
+          <ViewDirective option='Day' />
+        </ViewsDirective>
+        <Inject services={[Day, Week, Month, ExcelExport]} />
       </ScheduleComponent>
     );
   }
