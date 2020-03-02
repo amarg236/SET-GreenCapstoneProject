f776e85a3ab5523d8f01fe652b39967e64e581d4
Sonam Gurung <gurungamar25@gmail.com>
2020-03-01 19:18:28 -0600

    fixed the bug with dev tools

--- front-end-with-react/src/index.js
+++ front-end-with-react/src/index.js
@@ -7,17 +7,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
