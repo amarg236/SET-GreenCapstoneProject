d830b7958fa1d57d7d15f5736777530c1347c3b7
Sonam Gurung <gurungamar25@gmail.com>
2020-03-22 04:49:20 -0500

    Invite user and Email Confirmation handle

---
+++ front-end-with-react/src/components/EmailConfirmation/ChangePassword.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import "../../App.css";
+import axios from "axios";
+import Authtoken from "../../Utility/AuthToken";
+import "../../stylesheets/layout.css";
+import { Layout } from "antd";
+import { Modal, Form, Input, Button, Checkbox } from "antd";
+
+import { connect } from "react-redux";
+const { Content } = Layout;
+
+class ChangePassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ""
+    };
+  }
+
+  login = e => {
+    e.preventDefault();
+    console.log(Authtoken.getBaseUrl());
+    const pwObj = {
+      email: this.props.username,
+      password: this.state.password
+    };
+    axios
+      .post(Authtoken.getBaseUrl() + "/api/auth/setPassword", pwObj, {
+        headers: {
+          Authorization: "Bearer " + Authtoken.getUserInfo().token.split(" ")[1]
+        }
+      })
+      .then(res => {
+        console.log(res);
+        this.success();
+      });
+  };
+
+  success = () => {
+    Modal.success({
+      content: "Password has been changed successfully"
+    });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const layout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 6 }
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 8, span: 16 }
+    };
+    return (
+      <Content
+        className="site-layout-background"
+        style={{
+          padding: 24,
+          margin: 0,
+          minHeight: 580
+        }}
+      >
+        <Form
+          {...layout}
+          name="changePassword"
+          initialValues={{ remember: true }}
+        >
+          <Form.Item label="Username" name="username">
+            <Input
+              name="username"
+              type="text"
+              disabled
+              placeholder={this.props.username}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="New Password"
+            name="password"
+            rules={[
+              { required: true, message: "Please input your new password!" }
+            ]}
+          >
+            <Input.Password
+              autoFocus=""
+              value={this.state.password}
+              placeholder="Password"
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" onClick={this.login}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Content>
+    );
+  }
+}
+
+const mapStatetoProps = state => {
+  return {
+    username: state.userReducer.username
+  };
+};
+
+export default connect(mapStatetoProps)(ChangePassword);
