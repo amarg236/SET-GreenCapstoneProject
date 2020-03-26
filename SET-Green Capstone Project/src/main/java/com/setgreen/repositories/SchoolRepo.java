3ad14f90e7e2dcca9f7d38b0367440a023206c98
brendon.lebaron <brendon.lebaron@gmail.com>
2020-03-25 20:43:50 -0500

    Fix getting schoools by district, add typos to commit message

--- SET-Green Capstone Project/src/main/java/com/setgreen/repositories/SchoolRepo.java
+++ SET-Green Capstone Project/src/main/java/com/setgreen/repositories/SchoolRepo.java
@@ -1,8 +1,6 @@
 package com.setgreen.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.setgreen.model.School;
@@ -10,7 +8,6 @@ import com.setgreen.model.School;
 @Repository
 public interface SchoolRepo extends CrudRepository<School, Long>{
 
-	@Query("SELECT s FROM School s WHERE districtName = (:nm)")
-	Iterable<School> findInDistrict(@Param("nm") String nm);
+	Iterable<School> findByDistrict_Id(Long id);
 	
 }
