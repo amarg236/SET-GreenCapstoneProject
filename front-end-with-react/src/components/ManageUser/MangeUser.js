899cb51c6e8730bcd0530dd16722aa2401848ff8
Sonam Gurung <gurungamar25@gmail.com>
2020-04-05 17:59:05 -0500

    Changed the layout for Mange user and implemented functionality of deleting unverified user

--- front-end-with-react/src/components/ManageUser/MangeUser.js
+++ front-end-with-react/src/components/ManageUser/MangeUser.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import "../../stylesheets/createGame.css";
 import axios from "axios";
 import Authtoken from "../../Utility/AuthToken";
-import { Row, Col, Button } from "antd";
+import { Modal, Button, PageHeader, Descriptions } from "antd";
 import { connect } from "react-redux";
+
+const renderContent = (details, column = 2) => (
+  <Descriptions size="small" column={column}>
+    <Descriptions.Item label="User Email">{details.email}</Descriptions.Item>
+  </Descriptions>
+);
+
+const Content = ({ children, extra }) => {
+  return (
+    <div className="content">
+      <div className="main">{children}</div>
+      <div className="extra">{extra}</div>
+    </div>
+  );
+};
 
 class ManageUser extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
-      users: []
+      users: [],
     };
   }
 
@@ -20,68 +35,74 @@ class ManageUser extends Component {
     axios
       .get(Authtoken.getBaseUrl() + "/api/admin/viewUnverifiedUser", {
         headers: {
-          Authorization: "Bearer " + Authtoken.getUserInfo().token.split(" ")[1]
-        }
+          Authorization:
+            "Bearer " + Authtoken.getUserInfo().token.split(" ")[1],
+        },
       })
-      .then(res => {
+      .then((res) => {
         console.log(res.data);
         this.setState({ users: res.data, loading: false });
       });
   }
 
+  deleteUser(id) {
+    console.log("this is my id");
+    console.log(id);
+    const emptyObj = {
+      id: id,
+    };
+
+    axios
+      .post(Authtoken.getBaseUrl() + "/api/admin/user/remove", emptyObj, {
+        headers: {
+          Authorization:
+            "Bearer " + Authtoken.getUserInfo().token.split(" ")[1],
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        this.deleteSuccess();
+      });
+  }
+
+  deleteSuccess = () => {
+    Modal.success({
+      content: "User has been deleted Successfully",
+    });
+  };
+
   render() {
     return (
-      <div>
-        <Row>
-          <Col style={{ textAlign: "center" }} span={24}>
-            <h4>Pending Games</h4>
-          </Col>
-        </Row>
-        <Row
-          style={{
-            padding: "10px",
-            color: "#006ca1",
-            backgroundColor: "#dddd"
-          }}
-        >
-          <Col lg={6} md={5} sm={5} xs={6}>
-            Id
-          </Col>
-          <Col lg={6} md={5} sm={5} xs={6}>
-            Email
-          </Col>
-          <Col lg={4} md={4} sm={4} xs={4}>
-            First Name
-          </Col>
-          <Col lg={4} md={4} sm={4} xs={4}>
-            Last Name
-          </Col>
-        </Row>
-        {this.state.users.map(details => {
+      <div
+        style={{
+          marginTop: "10px",
+          backgroundColor: "#ffff",
+          padding: "20px",
+          boxShadow: " 0 1px 4px rgba(0, 21, 41, 0.08)",
+        }}
+      >
+        <PageHeader>
+          <h4 style={{ textAlign: "center" }}>Unverified Users</h4>
+        </PageHeader>
+
+        {this.state.users.map((details) => {
           const { id, email, firstname, lastname } = details;
           if (id) {
             return (
-              <Row
-                rowkey={id}
-                style={{
-                  padding: "5px",
-                  marginTop: "2px",
-                  backgroundColor: "#ffff"
-                }}
+              <PageHeader
+                key={id}
+                className="site-page-header-responsive"
+                // onBack={() => window.history.back()}
+                title={firstname.concat(" ").concat(lastname)}
+                // subTitle={time}
+                extra={[
+                  <Button key="1" onClick={() => this.deleteUser(id)}>
+                    Delete User
+                  </Button>,
+                ]}
               >
-                <Col lg={6} md={5} sm={5} xs={6}>
-                  {id}
-                </Col>
-                <Col lg={6} md={5} sm={5} xs={6}>
-                  {email}
-                </Col>
-                <Col lg={6} md={5} sm={5} xs={6}>
-                  {firstname}
-                </Col>
-                <Col lg={4} md={4} sm={4} xs={4}>
-                  {lastname}
-                </Col>
-              </Row>
+                <Content>{renderContent(details)}</Content>
+              </PageHeader>
             );
           }
         })}
