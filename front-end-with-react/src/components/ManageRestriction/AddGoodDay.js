f1e8af7d2a35cd8823e2dd05f26fa17487af363b
Sonam Gurung <gurungamar25@gmail.com>
2020-04-27 21:30:54 -0500

    Good Day

--- front-end-with-react/src/components/ManageRestriction/AddGoodDay.js
+++ front-end-with-react/src/components/ManageRestriction/AddGoodDay.js
@@ -1,101 +1,61 @@
-import "../../App.css";
-import React, { Component } from "react";
-
 import axios from "axios";
-import Authtoken from "../../Utility/AuthToken";
 import { connect } from "react-redux";
 import moment from "moment";
-import { SearchOutlined, FilterOutlined } from "@ant-design/icons";
+import Authtoken from "../../Utility/AuthToken";
+import React, { Component } from "react";
 import {
-  Tabs,
-  Row,
-  Layout,
-  Col,
-  Button,
-  Table,
-  Statistic,
-  Descriptions,
-  Input,
-  DatePicker,
-  Tag,
-  Modal,
-  Calendar,
-  Alert,
   Form,
+  Typography,
+  Input,
+  Button,
+  DatePicker,
   Checkbox,
+  Select,
+  Layout,
+  Modal,
 } from "antd";
+
+import { FormOutlined } from "@ant-design/icons";
+// import { PlusOutlined } from "@ant-design/icons";
 const { Content } = Layout;
-
-function onPanelChange(value, mode) {
-  console.log(value, mode);
-}
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 8 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
-
+const { TextArea } = Input;
+const { Title } = Typography;
 class AddGoodDay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      game: [],
-      school: [],
-      isRejected: null,
-      bulkAccept: false,
-      refresh: false,
-      value: moment("2017-01-25"),
-      selectedValue: moment("2017-01-25"),
-    };
-  }
+  state = {
+    homeTeam: "",
+    homeTeamId: "",
+    homeTeamObj: [],
+    awaySchoolObj: [],
+    awaySchoolList: [],
+    awaySchoolTeamList: [],
+    selectedDistrict: {
+      id: "",
+      districtName: "",
+    },
+    gameDate: moment().format("YYYY-MM-DD"),
+    gameStartTime: moment().format("HH:mm"),
+    gameEndTime: moment().format("HH:mm"),
+    // gameEndTime: moment()
+    //   .add(30, "minute")
+    //   .format("HH:mm"),
+    gameLocation: "",
+    againstTeam: "",
+    awayteamId: "",
+    againstSchool: "",
+    againstTeamDistrict: "",
+    againstTeamDistrictId: "",
+    gameTime: "",
+  };
 
   componentDidMount() {
     this.fetchApi();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.refresh != this.state.refresh) {
-      this.fetchApi();
-    }
-  }
-
-  fetchApi = () => {
-    let myTeamId = new Map();
-    this.props.myTeamId.map((row, index) => myTeamId.set(row));
-
-    const currentSchool = {
-      id: this.props.mySchool.id,
-    };
-
-    axios
-      .post(
-        Authtoken.getBaseUrl() + "/api/game/get/BySchool/notAccepted",
-        currentSchool,
-        {
-          headers: {
-            Authorization:
-              "Bearer " + Authtoken.getUserInfo().token.split(" ")[1],
-          },
-        }
-      )
-      .then((res) => {
-        // console.log("current school teams");
-        // console.log(res.data.result);
-        // console.log("length here");
-        // console.log(res.data.result.length);
-        let myData = res.data.result.filter(function (myGames) {
-          return myTeamId.has(myGames.awayteamId);
-        });
-        // console.log(myData);
-        // this.setState({
-        //   game: processData(myData),
-        // });
-      });
-  };
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.refresh != this.state.refresh) {
+  //     this.fetchApi();
+  //   }
+  // }
 
   successMsg = (s_message) => {
     Modal.success({
@@ -117,71 +77,170 @@ class AddGoodDay extends Component {
     });
   };
 
+  fetchApi = () => {
+    let ben = this.props.mySchool.id;
+
+    function getTeam() {
+      const forTeam = {
+        id: ben,
+      };
+      return axios.post(
+        Authtoken.getBaseUrl() + "/api/team/get/bySchool",
+        forTeam,
+        {
+          headers: {
+            Authorization:
+              "Bearer " + Authtoken.getUserInfo().token.split(" ")[1],
+          },
+        }
+      );
+    }
+
+    axios.all([getTeam()]).then(
+      axios.spread((getHomeTeamResponse) => {
+        // Both requests are now complete
+
+        this.setState({ homeTeamObj: getHomeTeamResponse.data.result });
+      })
+    );
+  };
+
+  onTitleChange = (e) => {
+    e.persist();
+    this.setState({ title: e.target.value });
+    console.log(e);
+  };
+
+  onDescriptionChange = (e) => {
+    e.persist();
+    this.setState({ description: e.target.value });
+    console.log(e);
+  };
+
+  success = () => {
+    Modal.success({
+      content: "Notice has been saved",
+    });
+  };
+  handleHomeTeam = (value) => {
+    const passedValue = JSON.parse(value);
+    console.log(passedValue);
+    this.setState({ homeTeam: passedValue.tmName });
+    this.setState({ homeTeamId: passedValue.id });
+  };
+
+  onChangeGameDate(date, dateString) {
+    // console.log(date);
+    console.log(dateString);
+    // this.setState({ gameDate: dateString });
+    this.setState({ gameDate: date?.format("YYYY-MM-DD") });
+  }
+
   render() {
-    const onFinish = (values) => {
-      console.log("Success:", values);
+    const layout = {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 8 },
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 10, span: 8 },
+    };
+
+    const onFinish = () => {
+      //   e.preventDefault();
+      console.log("Success:");
+      const noticeObj = {
+        title: this.state.title,
+        description: this.state.description,
+      };
+      console.log(noticeObj);
+      axios
+        .post(Authtoken.getBaseUrl() + "/api/notice/add", noticeObj, {
+          headers: {
+            Authorization:
+              "Bearer " + Authtoken.getUserInfo().token.split(" ")[1],
+          },
+        })
+        .then((res) => {
+          this.success();
+          // window.alert("The notice has been added successfully!!");
+          window.location.reload();
+        });
     };
 
     const onFinishFailed = (errorInfo) => {
       console.log("Failed:", errorInfo);
     };
+
     return (
       <Content
-        className="mediaCG"
+        className="mediaAS"
         style={{
           padding: 24,
           margin: 0,
           minHeight: 580,
         }}
       >
-        <Row style={{ backgroundColor: "#ffff" }}>
-          <Col span={16}>
-            <div
-              style={{
-                backgroundColor: "#ffff",
-                padding: "20px",
-                boxShadow: " 0 1px 4px rgba(0, 21, 41, 0.08)",
-              }}
+        <Title>
+          Add Good Days&nbsp;
+          <FormOutlined />
+        </Title>
+        <div
+          style={{
+            backgroundColor: "#ffff",
+            padding: "20px",
+            boxShadow: " 0 1px 4px rgba(0, 21, 41, 0.08)",
+          }}
+        >
+          <Form
+            {...layout}
+            name="notice-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item label="Select Team" name="homeTeam">
+              <Select
+                size="large"
+                defaultValue="Select Options"
+                style={{ width: 280 }}
+                onChange={this.handleHomeTeam}
+              >
+                {this.state.homeTeamObj.map((homeTeamDetails) => (
+                  <Select.Option
+                    key={homeTeamDetails.id}
+                    // value={index}
+                    value={JSON.stringify(homeTeamDetails)}
+                  >
+                    {homeTeamDetails.tmName}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              name="gameDate"
+              label="Choose Date"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
             >
-              <Form {...layout} name="nest-messages" onSubmit={this.gameSubmit}>
-                <Form.Item
-                  name="gameDate"
-                  label="Selected Date"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  name="gametime"
-                  label="Description"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-              </Form>
-            </div>
-          </Col>
-          <Col span={8} md={8} xs={0}>
-            <div
-              style={{
-                textAlign: "center",
-                padding: "30px",
-              }}
-            >
-              <Alert message="Choosen Date: YY-MM-DD" type="success" />
-              <hr />
-              <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-            </div>
-          </Col>
-        </Row>
+              <DatePicker
+                style={{ width: 280 }}
+                value={this.state.gameDate}
+                onChange={this.onChangeGameDate}
+                // defaultValue={moment("2020-03-08", this.dateFormat)}
+                format={this.dateFormat}
+              />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </Content>
     );
   }
@@ -189,11 +248,12 @@ class AddGoodDay extends Component {
 
 const mapStatetoProps = (state) => {
   return {
+    currentUser: state.userReducer.username,
     token: state.userReducer.token,
+    ifcollapsed: state.userReducer.sidebarCollapased,
     mySchool: state.userReducer.mySchool,
     schoolDistrict: state.userReducer.schoolDistrict,
-    userGameRedux: state.gameReducer.userGame,
-    myTeamId: state.gameReducer.myTeam,
   };
 };
-export default connect(mapStatetoProps, null)(AddGoodDay);
+
+export default connect(mapStatetoProps)(AddGoodDay);
